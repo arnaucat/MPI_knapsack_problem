8748311675ebe048aa313c43a7073d987f39e7bb
MEUSBURGGER <maicolmeusburger@gmail.com>
2017-06-03 10:59:01 +0200

    update version MPI

--- knapsackDYN_mpi.c
+++ knapsackDYN_mpi.c
@@ -1,122 +1,133 @@
-// A Dynamic Programming based solution for 0-1 Knapsack problem
+
+/*** 
+ * Part: MPI Implementation for 0-1 Knapsack problem 
+ * MPI Dynamic Programming based solution 
+ */
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <mpi.h>
-MPI_Status status,status2; // store status of a MPI_Recv
-MPI_Request request_snd,request_rcv; //capture request of a MPI_Isend
+#include <string.h>
 
 // A utility function that returns maximum of two integers
 int max(int a, int b) { return (a > b)? a : b; }
 
+
 // Returns the maximum value that can be put in a knapsack of capacity W
-static void knapSack(long int W, long int N, int wt[], int val[],int rank,int size,int *K[])
+static int knapSack(long int W, long int N, int wt[], int val[], double* timeSpent)
 {
-
-	long int i, w,j;
-	int rcv_rank = 0; //k-1)%size;// receive data
- 	int snd_rank = 1; //(rank+1)%size;// send data   
- 	int low_bound; //low bound of the number of rows  allocated to a slave
-	int upper_bound; //upper bound of the number of rows  allocated to a slave
-	int portion; //portion of the number of rows allocated to a slave
-
-	// Build table K[][] in bottom up manner
-	for (i = 0; i <= N; i++)
-	{
-
-		//printf("iteracion : %ld \n",i);	
-		//rcv_rank = (rank-1)%size;   //rank to receive data
- 	  	//snd_rank = (rank+1)%size; // rank to send data
-
-		if (rank == 0) 
-		 	rcv_rank=size-1;
-	    else            
-	    	rcv_rank=rank-1;
-	    //Define the destiny the rigth neighbour
-	    if(rank==size-1) 
-	    	snd_rank=0;
-	    else                 
-	    	snd_rank=rank+1;
-
- 	  	/*if(rcv_rank < 0)
- 	  	{
- 	  		rcv_rank = size-1;
- 	  	}*/	  	
-		
-	    
-
-	    portion = (W/ (size )); // calculate portion without master
-		low_bound = (rank) * portion;
-		if (((rank +1) == size) && ((W% (size - 1)) != 0)) {//if rows of [A] cannot be equally divided among slaves
-			upper_bound =W; //last slave gets all the remaining rows
-		} else {
-			upper_bound = low_bound + portion; //rows of [A] are equally divisable among slaves
-		}		
-
-		for (w = low_bound; w < upper_bound; w++)
-		{
-			if (i==0 || w==0)
-				K[i][w] = 0;
-			else if (wt[i-1] <= w)
-				K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]);
-			else
-				K[i][w] = K[i-1][w];
-
-			printf("position %ld iteracion %ld low_bound %d upper_bound %d core %d calculo : %i \n",i,w, low_bound,upper_bound,rank,K[i][w]);				
-						
-		}	   
-		
-		//printf("envia a : %d iteracion %ld \n",snd_rank,i);	
-		MPI_Isend(K[i],upper_bound, MPI_INT, snd_rank, 0, MPI_COMM_WORLD,&request_snd);	
 	
-		
-		if(rank==0 && rcv_rank==0)
-		{
-			MPI_Recv(K[i], W , MPI_INT, rcv_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
-		}
+	long int i, w;	
+    int result=0;     
+ 	int  size,rank,slave;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+     
+    
+    //initial time
+	double start=MPI_Wtime();
 
-		if(i > 0)
-		{
-			//printf("Recibe de : %d \n",rcv_rank);	
-			//printf("recibe iteracion : %ld rcv_rank %d snd_rank %d \n",i,rcv_rank,snd_rank);	
-			MPI_Recv(K[i-1], W , MPI_INT, rcv_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);		
-			MPI_Wait(&request_snd, &status2);
-		}			
+     /* master initializes work*/
+	if (rank==0){
+        //define the matrix.
+        int K[1][W+3] ;
+		int i,count;
+        int m[W+3];
+        memset(K[0], 0, (W+3)*sizeof(int));	
+		
+        
+		for(i=1; i<=N ; i++)
+		{			
+			for (slave=1;slave<size ;slave++)  
+     			MPI_Send((void*)K[0], W+1, MPI_INT, slave, 1, MPI_COMM_WORLD);               
+
 			
-		
+			for(count = 1; count< size; count++)
+			{     		
+                //recive the vector
+				MPI_Recv((void*)m, W+3, MPI_INT, MPI_ANY_SOURCE, 10, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                //allocate in the matrix.
+                memcpy(K[0]+m[W+1], m+m[W+1], sizeof(int) * (m[W+2]-m[W+1])+sizeof(int));                
+            }		
+		}	
+        
+        result=K[0][W];
 	}
-	if(rank == 0)
-	{						
-		MPI_Recv(K[i-1], W, MPI_INT, rcv_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
-		MPI_Wait(&request_snd, &status2);		
-	}  
-	
+    /* work done by slaves*/
+	else{    
+        
+        int  Kp[2][W+3];
+     	int k,portion,low_bound,upper_bound;       
+        
+		for(k=1;k<=N;k++)
+     	{	
+        	i=1;
+            //receive the message from the master
+			MPI_Recv((void*)Kp[i-1], W+1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+         	
+            // calculate portion without master
+            portion = (W/ (size - 1)); 
+            low_bound = (rank - 1) * portion;
+            //if rows of [K] cannot be equally divided among slaves
+            if (((rank + 1) == size) && ((W% (size - 1)) != 0)) {
+                //last slave gets all the remaining rows
+                upper_bound =W; 
+            } else {
+                //rows of [K] are equally divisable among slaves
+                upper_bound = low_bound + portion; 
+            }
+
+            //printf("low_bound %d  upper_bound %d \n",low_bound,upper_bound);
+
+            // each core only computes a part of the data.
+            
+	  		for (w =low_bound; w <= upper_bound; w++)
+			{
+				if (i==0 || w==0)
+					Kp[i][w] = 0;
+				else if (wt[k-1] <= w)
+					Kp[i][w] = max(val[k-1] + Kp[i-1][w-wt[k-1]], Kp[i-1][w]);
+				else
+					Kp[i][w] = Kp[i-1][w];
+            }
+            //Initial and final 'w' per core
+            Kp[i][W+1]=low_bound;
+            Kp[i][W+2]=upper_bound;
+            //Send a message 
+            MPI_Send((void*)Kp[i], W+3, MPI_INT, 0, 10, MPI_COMM_WORLD);   		 
+        }         
+	}
+    //compute the time spent in the parallelization
+    (*timeSpent)=MPI_Wtime()-start;
+
+    MPI_Finalize();   
+
+    return result;
 
 }
 
 int main(int argc, char **argv){
 
     FILE *test_file;
-    
+    double start_time, elapsed_time; /* for time measurement */
+
     int *val, *wt;   // width and cost values
     long int Nitems; // Number of items
     long int Width;  // Max. load to carry
     long int cont;    // counter
     double tpivot1=0,tpivot2=0,tpivot3=0; //time counting
     struct timeval tim;
-    int **K;
-    int rank; //process rank
-	int size; //number of processes
-	int i,w;
-   
 
 	if (argc!=2) {
 	  printf("\n\nError, mising parameters:\n");
 	  printf("format: test_file \n");	 
 	  return 1;
-	} 
-	
+	}
+    
+
     //Capture first token time - init execution
     gettimeofday(&tim, NULL);
     tpivot1 = tim.tv_sec+(tim.tv_usec/1000000.0);
@@ -127,60 +138,39 @@ int main(int argc, char **argv){
 	}
 
     //Reading number of items and Maximum width
-	fscanf(test_file,"%ld %ld\n",&Nitems, &Width);   
-
-		
+	fscanf(test_file,"%ld %ld\n",&Nitems, &Width);
+    
+    //printf("%ld\n",Nitems*sizeof(int));
+	double timeSpent=0;
 	val = (int *)malloc(Nitems*sizeof(int)); //values for each element
-	wt = (int *)malloc(Nitems*sizeof(int)); //width  for each element
+	 wt = (int *)malloc(Nitems*sizeof(int)); //width  for each element
     
     //Reading value and width for each element
 	for (cont=0;cont<Nitems;cont++){
 	  fscanf(test_file,"%d,%d\n",&val[cont],&wt[cont]);
 	}
 
-
-
-	K = (int**) malloc((Nitems+1)*sizeof(int*));
-	for (i=0;i<=Nitems;i++)
-		K[i] = (int*)malloc((Width+1)*sizeof(int));	
-
-
-	MPI_Init(&argc, &argv); //initialize MPI operations
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); //get the rank
-    MPI_Comm_size(MPI_COMM_WORLD, &size); //get number of processes
-
-    //gettimeofday(&tim, NULL);
-    tpivot2 = MPI_Wtime();    
-    knapSack(Width,Nitems, wt, val,rank,size,K);
-    MPI_Barrier(MPI_COMM_WORLD);
+     //    printf("\n\n ---- Results TCapacity:items:Tcost:toptim:ttime ----\n\n");
+    gettimeofday(&tim, NULL);
+    tpivot2 = (tim.tv_sec+(tim.tv_usec/1000000.0));
+    //printf("HOLA2\n");
     
-	if(rank == 0)
-	{
-		printf("%ld:%ld:%d",Width,Nitems, K[Nitems][Width] );
-	    gettimeofday(&tim, NULL);
-	    tpivot3 = (tim.tv_sec+(tim.tv_usec/1000000.0));
-	 	printf(":%.6lf:%.6lf\n", tpivot3-tpivot2,tpivot3-tpivot1); 
+    //int K[Nitems+1][Width+1] ;
+    int result=  knapSack(Width,Nitems, wt, val,&timeSpent);
+      // we only print the result for the core with result greater than 0 (core 0).
+      if (result>0){
+         printf("%ld:%ld:%d", Width, Nitems, result);
 
+    	gettimeofday(&tim, NULL);
 
-	 	for (i = 0; i <= Nitems; i++)
-	    {
-	        for (w = 0; w <= Width; w++)
-	            printf("%3d ",K[i][w]);
-	        printf("\n");
-	    }
+    	tpivot3 = (tim.tv_sec+(tim.tv_usec/1000000.0));
+   		 printf(":%.6lf:%.6lf\n", timeSpent,tpivot3-tpivot1);
+   }
 
-	    free(val);
-		free(wt);
-		free(K);
-		fclose(test_file); 
-	 	
-	 	MPI_Wait(&request_snd, &status2);
-		
-    }
-
-    
+	free(val);
+	free(wt);
 	
+	fclose(test_file);
 
-	MPI_Finalize();
 	return 0;
 }
